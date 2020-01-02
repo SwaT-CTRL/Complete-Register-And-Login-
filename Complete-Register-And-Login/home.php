f9d40f6337543311ddb3125d54af4480c55818f6
SwaT-CTRL <59417168+SwaT-CTRL@users.noreply.github.com>
2020-01-02 21:11:26 +0530

    Add files via upload

---
+++ Complete-Register-And-Login/home.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['username'])){
+    header('location:login.php');
+}
+
+?>
+
+
+
+
+<html>
+<head>
+<title>Home Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+    <a class="float-right" href="logout.php">LOGOUT</a>
+    <h1>Welcome <?php echo $_SESSION['username']; ?></h1>
+    </div>
+</body>
+</html>
